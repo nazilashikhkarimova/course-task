33867c59dceaf11e2f00e2843ef6e31ce77a4815
Nazil <nazilakarimova@gmnail.com>
2023-05-31 20:33:36 +0400

    second commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Open the login popup
+function openLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'block';
+}
+
+// Close the login popup
+function closeLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'none';
+}
+
